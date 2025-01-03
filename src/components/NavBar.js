559dccba67b429f990ab6450e4a8ae0705e898af
Maximiliano Pereyra <maximilianop98@gmail.com>
2025-01-03 20:38:45 -0300

    saludo usuario

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,18 @@
-// NavBar: Barra de navegación con un botón para cerrar sesión.
-
 import React from 'react';
 
-const NavBar = ({ handleLogout }) => {
+const NavBar = ({ handleLogout, username }) => {
   return (
     <div className="bg-blue-600 p-4 flex justify-between items-center text-white shadow-md rounded-md">
       <h1 className="text-2xl font-bold">Gestión de Tareas</h1>
-      <button
-        onClick={handleLogout}
-        className="py-2 px-4 bg-red-600 hover:bg-red-700 rounded-md shadow"
-      >
-        Cerrar Sesión
-      </button>
+      <div className="flex items-center">
+        <span className="mr-4">Hola, {username}</span>
+        <button
+          onClick={handleLogout}
+          className="py-2 px-4 bg-red-600 hover:bg-red-700 rounded-md shadow"
+        >
+          Cerrar Sesión
+        </button>
+      </div>
     </div>
   );
 };
