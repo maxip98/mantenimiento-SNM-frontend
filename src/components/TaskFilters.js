4b6d6a6591f6f53dcac16838c9ef542f9eccce57
Maximiliano Pereyra <maximilianop98@gmail.com>
2025-01-03 21:03:27 -0300

    saludo usuario

--- src/components/TaskFilters.js
+++ src/components/TaskFilters.js
@@ -11,8 +11,8 @@ const TaskFilters = ({ filters, setFilters, applyFilters }) => {
           className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
         >
           <option value="">Todos</option>
-          <option value="local1">Say No More</option>
-          <option value="local2">Say Wich</option>
+          <option value="Say No More">Say No More</option>
+          <option value="Say Wich">Say Wich</option>
         </select>
       </div>
       <div className="mb-4">
